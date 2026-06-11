99bbd8ed846546d14785959fce9987994f55f355
Mauricio Villegas <mauricio.villegas@example.com>
2026-06-10 21:46:58 -0400

    Initial Commit

---
+++ SshLauncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SshLauncher
+//
+//  Created by Mauricio Villegas on 10/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SshLauncher/SshLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SshLauncherApp.swift
+//  SshLauncher
+//
+//  Created by Mauricio Villegas on 10/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SshLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
